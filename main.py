cc473c00c002e1af122f1ccd82b1e8a50e62ea97
darknuma <aminuemmazon@gmail.com>
2025-01-01 16:48:57 +0100

    Project commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import websocket
+
+
+load_dotenv()
+
+
+json = {
+    "Name": "Emmanuel",
+    "Age": 32,
+    "Nationality": "Nigeria",
+    "hobbies": ['Reading', "Music", "OS", "oblee"] 
+}
---
+++ src/scrape_stream.py
@@ -0,0 +1,19 @@
+import polars as pl
+import websockets
+import selenium
+
+# we can use the snowflake datawarehouse
+# kafka library 
+# what orchestration tool to use, my choice here is MageAI
+# IAAC with Terraform
+# quix or bytewaxx? 
+# using grafana 
+
+# we arre going to use apache flink
+
+#  we can think
+
+# webdriver = selenium
+
+# url = ""
+
